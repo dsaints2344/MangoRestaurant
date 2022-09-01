0429d93aac63c4f7bf7de6a0d4ae9769e73d3093
David De Los Santos <ddelossantosmateo@gmail.com>
2022-09-01 14:32:49 -0400

    Creating repo and dtos

--- Mango.Services.ProductAPI/DbContexts/Models/Dto/ResponseDto.cs
+++ Mango.Services.ProductAPI/DbContexts/Models/Dto/ResponseDto.cs
@@ -2,5 +2,8 @@ namespace Mango.Services.ProductAPI.DbContexts.Models.Dto;
 
 public class ResponseDto
 {
-    
+    public bool IsSuccess { get; set; } = true;
+    public object Result { get; set; }
+    public string DisplayMessage { get; set; } = "";
+    public List<string> ErrorMessages { get; set; }
 }
--- Mango.Services.ProductAPI/Repository/IRepository.cs
+++ Mango.Services.ProductAPI/Repository/IRepository.cs
@@ -1,6 +1,11 @@
+using Mango.Services.ProductAPI.DbContexts.Models.Dto;
+
 namespace Mango.Services.ProductAPI.Repository;
 
 public interface IRepository
 {
-    
+    Task<IEnumerable<ProductDto>> GetProducts();
+    Task<ProductDto> GetProductById(int productId);
+    Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+    Task<bool> DeleteProduct(int productId);
 }
